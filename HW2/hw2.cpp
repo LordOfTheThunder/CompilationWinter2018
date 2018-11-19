faeb09f425ce8c78922e96142fe33d0df00a9111
Maxim Kolchinsky <maxim.kolchinsky@intel.com>
2018-11-19 23:28:02 +0200

    added hw2.cpp

---
+++ HW2/hw2.cpp
@@ -0,0 +1,35 @@
+#include "hw2.h"
+#include <map>
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+
+using std::vector;
+using std::set;
+using std::map;
+typedef map<nonterminal , map<tokens, int> > MType;
+
+#define ERROR -1
+
+void compute_nullable() {
+    vector<nonterminal> v;
+    for (int i = S; i < NONTERMINAL_ENUM_SIZE; ++i) {
+			        
+    }
+}
+
+void compute_first() {
+
+}
+
+void compute_follow() {
+
+}
+
+void compute_select() {
+
+}
+
+void parser() {
+
+}
