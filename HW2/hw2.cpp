32858c36d5f296781cad027b4bf87db3d00444dd
Maxim Kolchinsky <maxim.kolchinsky@intel.com>
2018-11-20 23:18:43 +0200

    Added implementation to compute_nullable

--- HW2/hw2.cpp
+++ HW2/hw2.cpp
@@ -12,10 +12,24 @@ typedef map<nonterminal , map<tokens, int> > MType;
 #define ERROR -1
 
 void compute_nullable() {
-    vector<nonterminal> v;
-    for (int i = S; i < NONTERMINAL_ENUM_SIZE; ++i) {
-			        
-    }
+    vector<bool> v(NONTERMINAL_ENUM_SIZE, false);
+	// Loop through all layers of the matrix
+	for (int i = S; i < NONTERMINAL_ENUM_SIZE; ++i) {
+		// Loop through all possible grammars
+		for (int j = 0; j < grammar.size(); j++) {
+			// Loop through all terminals of each rule
+			bool is_nullable = true;
+			for (int k = 0; k < grammar[j].rhs.size(); k++) {
+				if (grammar[j].rhs[k] > NONTERMINAL_ENUM_SIZE || v[grammar[j].rhs[k]] == false) {
+					is_nullable = false;
+				}
+			}
+			if (is_nullable) {
+				v[grammar[j].lhs] = true;
+			}
+		}
+	}
+	print_nullable(v);
 }
 
 void compute_first() {
