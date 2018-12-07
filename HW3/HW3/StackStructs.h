85aad16bc771cbe713526598092a9c1f063f2123
Max Kolchinsky <max.kolchinsky@gmail.com>
2018-12-07 22:50:11 +0200

    fixed structs as parameters to functions

--- HW3/HW3/StackStructs.h
+++ HW3/HW3/StackStructs.h
@@ -70,7 +70,9 @@ struct StackType {
 	types type;
 	vector<varPair> func_info;
 	types ret_type;
-	StackType(types type = types_Undefined, string str = "") : type(type), str(str), lineno(1), func_info() {
+	string struct_type;
+	StackType(types type = types_Undefined, string str = "") : type(type), str(str), lineno(1),
+		func_info(), struct_type("") {
 		ret_type = types_Undefined;
 	}
 };
