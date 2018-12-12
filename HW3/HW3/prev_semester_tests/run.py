4270e07f68088911149bc13eaed45c8b32688791
Eitan <eitan.kosman@gmail.com>
2018-12-12 16:02:10 +0200

    added maayan's tests

---
+++ HW3/HW3/prev_semester_tests/run.py
@@ -0,0 +1,39 @@
+import os
+import re
+from subprocess import Popen
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-stop_on_fail', default='true')
+parser.add_argument('-diff', default='true')
+args = parser.parse_args()
+
+in_pattern = r'(.*).in'
+in_files = []
+for file in os.listdir('.'):
+    res = re.search(in_pattern, file)
+    if res:
+        in_files.append(res.group(1))
+
+for test in in_files:
+    test_name = test + '.in'
+    print "Running " + test_name,
+    output = Popen([r"./../hw3 < " + test_name], stdout=subprocess.PIPE, shell=True).stdout.read()
+    with open(test + '.out') as fp:
+        expected = fp.read()
+        if expected == output:
+            print('PASSED')
+        else:
+            if args.diff == 'true':
+                print('FAILED')
+                print ("Your output:\n")
+                print("=================")
+                print(output)
+                print("=================")
+                print("Expected output:")
+                print("=================")
+                print(expected)
+                print("=================")
+            if args.stop_on_fail == 'true':
+                break
